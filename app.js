e61745152339a50d46fdccad9d573409ac4025f3
bennyc-seamlessgroup <benny.cheung@seamlessgroup.com>
2026-04-08 10:38:58 +0800

    Update webhook URL in fetch request

--- app.js
+++ app.js
@@ -195,7 +195,8 @@ els.form.addEventListener("submit", async function (e) {
   };
 
   try {
-    const res = await fetch("https://oval-ambien-coupons-gba.trycloudflare.com/webhook/booking-submit", {
+    const res = await fetch("https://impose-stories-powerpoint-uni.trycloudflare.com/webhook-test/booking-submit
+", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
